dd4621e6d59babba707921f3f84c8ff562c86178
hejun <pro_jerry@163.com>
2016-03-11 14:03:33 +0800

    weixin-demo

--- weixin/src/main/java/com/weixin/controller/WechatServlet.java
+++ weixin/src/main/java/com/weixin/controller/WechatServlet.java
@@ -1,16 +1,14 @@
 package com.weixin.controller;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.util.Arrays;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.weixin.util.WechatProcess;
+import com.weixin.util.SHA1;
 
 
 /**
@@ -21,36 +19,31 @@ import com.weixin.util.WechatProcess;
 @SuppressWarnings("serial")
 public class WechatServlet extends HttpServlet{
 
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
 	
-		request.setCharacterEncoding("utf-8");
-		response.setCharacterEncoding("utf-8");
-		
-		 /** 读取接收到的xml消息 */  
-        StringBuffer sb = new StringBuffer();  
-        InputStream is = request.getInputStream();  
-        InputStreamReader isr = new InputStreamReader(is, "UTF-8");  
-        BufferedReader br = new BufferedReader(isr);  
-        
-        String s = "";  
-        while ((s = br.readLine()) != null) {  
-            sb.append(s);  
-        }  
-        
-        String xml = sb.toString(); //次即为接收到微信端发送过来的xml数据  
-        String result = "";  
-        /** 判断是否是微信接入激活验证，只有首次接入验证时才会收到echostr参数，此时需要把它直接返回 */  
-        String echostr = request.getParameter("echostr");  
-        
-        if (echostr != null && echostr.length() > 1) {  
-            result = echostr;  
-        } else {  
-            //正常的微信处理流程  
-            result = new WechatProcess().processWechatMag(xml);  
-        }  
-        
+		// 自定义 token
+	    private String TOKEN = "weixin";
+
+	    public void doGet(HttpServletRequest request, HttpServletResponse response)
+	            throws ServletException, IOException {
+	        // 微信加密签名
+	        String signature = request.getParameter("signature");
+	        // 随机字符串
+	        String echostr = request.getParameter("echostr");
+	        // 时间戳
+	        String timestamp = request.getParameter("timestamp");
+	        // 随机数
+	        String nonce = request.getParameter("nonce");
+
+	        String[] str = { TOKEN, timestamp, nonce };
+	        Arrays.sort(str); // 字典序排序
+	        String bigStr = str[0] + str[1] + str[2];
+	        // SHA1加密
+	        String digest = new SHA1().getDigestOfString(bigStr.getBytes()).toLowerCase();
+
+	        // 确认请求来至微信
+	        if (digest.equals(signature)) {
+	            response.getWriter().print(echostr);
+	        }
         
 		
 	}
